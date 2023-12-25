d1fa90e1e769b6d4a762eaaee659de9b4958ca8b
cza14h <ziao.chen1995@gmail.com>
2023-12-25 23:52:23 +0800

    feat: add chat service impl

--- apps/chat/service/chat.go
+++ apps/chat/service/chat.go
@@ -1,1 +1,24 @@
 package service
+
+import (
+	"context"
+	"sync"
+
+	"github.com/cza14h/nino-work/proto/chat"
+)
+
+type ChatServiceRpcImpl struct{}
+
+var once sync.Once
+var chatService *ChatServiceRpcImpl
+
+func GetChatServiceRpc() *ChatServiceRpcImpl {
+	once.Do(func() {
+		chatService = &ChatServiceRpcImpl{}
+	})
+	return chatService
+}
+
+func Chat(ctx context.Context, in *chat.ChatRequest, out *chat.ChatResponse) {
+
+}
