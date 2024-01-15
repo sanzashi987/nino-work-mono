9246ff0da5203ef29423ccb11bac9ba2d913d4b6
cza14h <43773895+cza14h@users.noreply.github.com>
2024-01-15 19:34:00 +0800

    feat: add canvas service bootstrap

--- apps/canvas-pro/bootstrap.go
+++ apps/canvas-pro/bootstrap.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"github.com/cza14h/nino-work/pkg/bootstrap"
+  "go-micro.dev/v4"
+	"go-micro.dev/v4/web"
+)
 
+func main() {
+	conf, etcdRegistry := bootstrap.CommonBootstrap("canvasService.client")
+	// dao.ConnectDB()
+
+	canvasService, ok := conf.Service["canvasService"]
+	if !ok {
+		panic("Canvas Service is not configured")
+	}
+
+	webService := web.NewService(
+		web.Name(canvasService.Name+".web"),
+		web.Address(bootstrap.GetAddress(canvasService.Host, canvasService.Port)),
+		// web.Handler(http.NewRouter(conf.System.LoginPage)),
+		web.Registry(etcdRegistry),
+	)
+
+	webService.Init()
+	webService.Run()
 }
