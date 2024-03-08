6388bf24884b632e866317a32b393d11d114a300
cza14h <ziao.chen1995@gmail.com>
2024-03-09 01:05:58 +0800

    feat: update project foreign key

--- apps/canvas-pro/db/dao/project.go
+++ apps/canvas-pro/db/dao/project.go
@@ -6,7 +6,6 @@ import (
 	"github.com/cza14h/nino-work/apps/canvas-pro/db/model"
 	"github.com/cza14h/nino-work/apps/canvas-pro/enums"
 	"github.com/cza14h/nino-work/pkg/db"
-	"gorm.io/gorm"
 )
 
 type ProjectDao struct {
@@ -17,25 +16,32 @@ func NewProjectDao(ctx context.Context) *ProjectDao {
 	return &ProjectDao{db.InitBaseDao[model.ProjectModel](ctx)}
 }
 
-
 func (dao *ProjectDao) GetList(page, size int, workspace string /**optional**/, name, group *string) (projects *[]model.ProjectModel, err error) {
-	groupModel := model.ProjectGroupModel{}
-	groupModel.Workspace = workspace
+
+	query := dao.DB.Scopes(db.Paginate(page, size)).Model(&model.ProjectModel{}).Where("workspace = ?", workspace)
 
 	if group != nil {
-		groupModel.Id, _, err = enums.GetIdFromCode(*group)
+		_, _, err = enums.GetIdFromCode(*group)
 		if err != nil {
 			return
 		}
+
+		query = query.Where(" group = ?", *group)
 	}
 
-	filterByName := func(db *gorm.DB) *gorm.DB {
-		res := db
-		if name != nil {
-			res = res.Where(" name LIKE ?", "%"+*name+"%")
-		}
-		return res
+	if name != nil {
+		query = query.Where(" group LIKE ?", *name)
 	}
-	err = dao.DB.Preload("Projects", filterByName).Scopes(db.Paginate(page, size)).Model(&groupModel).Association("Projects").Find(projects)
+	err = query.Find(projects).Error
 	return
+	// filterByName := func(db *gorm.DB) *gorm.DB {
+	// 	res := db
+	// 	if name != nil {
+	// 		res = res.Where(" name LIKE ?", "%"+*name+"%")
+	// 	}
+	// 	return res
+	// }
+	// err = dao.DB.Preload("Projects", filterByName).Scopes(db.Paginate(page, size)).Model(&groupModel).Association("Projects").Find(projects)
+	// return
+
 }
