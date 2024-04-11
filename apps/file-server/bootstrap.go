dd61f66a84e0e11758b38ee17bc661b7b58269db
cza14h <ziao.chen1995@gmail.com>
2024-04-11 23:52:10 +0800

    feat: add file service rpc

--- apps/file-server/bootstrap.go
+++ apps/file-server/bootstrap.go
@@ -1,14 +1,39 @@
 package main
 
-import "github.com/cza14h/nino-work/pkg/bootstrap"
+import (
+	"github.com/cza14h/nino-work/pkg/bootstrap"
+	"go-micro.dev/v4"
+)
 
 func main() {
 
 	conf, etcdRegistry := bootstrap.CommonBootstrap("fileService.client")
 
-	fileService, ok := conf.Service["fileService"]
+	fileServiceConf, ok := conf.Service["fileService"]
 	if !ok {
 		panic("File Service is not configured")
 	}
+
+	rpcService := micro.NewService(
+		micro.Name(fileServiceConf.Name),
+		micro.Address(bootstrap.GetAddress(fileServiceConf.Host, fileServiceConf.Port)),
+		micro.Registry(etcdRegistry),
+	)
+
+	// webService := web.NewService(
+	// 	web.Name(fileServiceConf.Name+".web"),
+	// 	web.Address(bootstrap.GetAddress(fileServiceConf.Host, fileServiceConf.Port)),
+	// 	web.Handler(http.NewRouter(conf.System.LoginPage)),
+	// 	web.Registry(etcdRegistry),
+	// )
+
+	// user.RegisterUserSerivceHandler(rpcService.Server(), service.GetUserServiceRpc())
+
+	// go func() {
+	// 	webService.Run()
+	// }()
+
+	rpcService.Init()
+	rpcService.Run()
 
 }
