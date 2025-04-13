f807e3cdb5f74182639368654db9e50d5a7ecd08
cza14h <ziao.chen1995@gmail.com>
2025-04-14 01:49:07 +0800

    feat: enhance RequestButton component to support icon mode and improve loading state handling

--- frontend/@nino-work/ui-components/src/RequestButton.tsx
+++ frontend/@nino-work/ui-components/src/RequestButton.tsx
@@ -1,9 +1,10 @@
-import { Button, ButtonProps } from '@mui/material';
+import { Button, ButtonProps, IconButton } from '@mui/material';
 import React, { createContext, useCallback, useContext, useMemo, useState } from 'react';
 
 // type ButtonProps = React.ComponentProps<typeof Button>['onClick'];
 
 export type RequestButtonProps = ButtonProps & {
+  mode?: 'button' | 'icon'
   onClick?: (e: React.MouseEvent) => Promise<any>
 };
 const LoadingContext = createContext<{ loading:boolean, setLoading:(loading:boolean)=>void } | null>(null);
@@ -15,12 +16,12 @@ export const LoadingGroup: React.FC<React.PropsWithChildren> = ({ children }) =>
 
   return (
     <LoadingContext.Provider value={ctx}>
-      { children}
+      {children}
     </LoadingContext.Provider>
   );
 };
 
-const RequestButton: React.FC<RequestButtonProps> = ({ onClick, ...rest }) => {
+const RequestButton: React.FC<RequestButtonProps> = ({ onClick, mode, children, ...rest }) => {
   const [loading, setLoading] = useState(false);
   const inCtx = useContext(LoadingContext);
   const handleClick = useCallback((e:React.MouseEvent) => {
@@ -31,7 +32,17 @@ const RequestButton: React.FC<RequestButtonProps> = ({ onClick, ...rest }) => {
     });
   }, [inCtx?.setLoading, onClick]);
 
-  return <Button {...rest} loading={inCtx?.loading ?? loading} onClick={handleClick} />;
+  const Com = mode === 'icon' ? IconButton : Button;
+
+  return React.createElement(
+    Com,
+    {
+      ...rest,
+      loading: inCtx?.loading ?? loading,
+      onClick: handleClick
+    },
+    children
+  );
 };
 
 export default RequestButton;
